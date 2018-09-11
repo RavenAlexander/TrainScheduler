76165b03b5c0e1b14d5be55ce2dded866c268ecc
ralexander <theravenalexander@gmail.com>
2018-09-10 20:40:13 -0600

    text changes

---
+++ assets/logic/logic.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAdoT8cxKI1QoJoI3PWGZzJDKeBDllfvi8",
+    authDomain: "trainscheduler-bd197.firebaseapp.com",
+    databaseURL: "https://trainscheduler-bd197.firebaseio.com",
+    projectId: "trainscheduler-bd197",
+    storageBucket: "trainscheduler-bd197.appspot.com",
+    messagingSenderId: "267071318324"
+  };
+  firebase.initializeApp(config);
